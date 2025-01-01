1046ecd97ea671b5a75c0f6cfede903ef3041555
danvosk <gorkem1928karagoz@gmail.com>
2025-01-01 18:12:34 +0300

    Gereksiz yorum satırları kaldırıldı

--- Seyahat Rehberim/CommentCell.swift
+++ Seyahat Rehberim/CommentCell.swift
@@ -5,23 +5,6 @@
 //  Created by Görkem Karagöz on 24.12.2024.
 //
 
-//import UIKit
-//
-//class CommentCell: UITableViewCell {
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        // Initialization code
-//    }
-//
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
-//
-//}
-
 import UIKit
 
 class CommentCell: UITableViewCell {
